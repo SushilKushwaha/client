1b1a44e3e0b79fcb2ffb62d650f3e7ec67d1742b
SushilKushwaha <sushilsinha733@gmail.com>
2024-02-03 21:16:11 +0545

    complete listing page

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -181,7 +181,9 @@ export default function CreateListing() {
                         <input type='number' id='regularPrice' min='1000' max='1000000000' required className='p-3 border border-gray-300 rounded-lg' onChange={handleChange} value={formData.regularPrice} />
                         <div className='flex flex-col items-center'>
                         <p>Regular Price</p>
+                        {formData.type === 'rent' && (
                         <span className='text-xs'>(Rs / month)</span>
+                        )}
                         </div>
                     </div>
                     {formData.offer && (
@@ -189,7 +191,9 @@ export default function CreateListing() {
                         <input type='number' id='discountPrice' min='0' max='1000000000' required className='p-3 border border-gray-300 rounded-lg' onChange={handleChange} value={formData.discountPrice} />
                         <div className='flex flex-col items-center'>
                         <p>Discounted Price</p>
-                        <span className='text-xs'>(Rs / month)</span>
+                        {formData.type === 'rent' && (
+                            <span className='text-xs'>(Rs / month)</span>
+                        )} 
                         </div>
                     </div>
                     )}
