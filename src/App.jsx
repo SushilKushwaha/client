bdd78aca101a862f4b20e7344e4ee89676c8bb60
SushilKushwaha <sushilsinha733@gmail.com>
2024-05-22 12:57:20 +0545

    About router error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-import About from './pages/about';
+import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
